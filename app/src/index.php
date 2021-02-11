7fd8db1f7e7a2eb989f39a8d4d5103b26aa64fcd
IkezoeMakoto <150283t@gmail.com>
2021-02-11 18:09:53 +0900

    見れるようになった

--- app/src/index.php
+++ app/src/index.php
@@ -4,6 +4,6 @@ tideways_xhprof_enable(TIDEWAYS_XHPROF_FLAGS_MEMORY | TIDEWAYS_XHPROF_FLAGS_CPU)
 phpinfo();
 
 file_put_contents(
-    sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid() . '.myapplication.xhprof',
+    sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'xhprof' . DIRECTORY_SEPARATOR . uniqid() . '.myapplication.xhprof',
     serialize(tideways_xhprof_disable())
 );
