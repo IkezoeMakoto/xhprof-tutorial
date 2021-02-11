5e5f4d139be17e4345191b264ccbe3f01419f275
IkezoeMakoto <150283t@gmail.com>
2021-02-11 16:53:21 +0900

    動かない

--- app/src/index.php
+++ app/src/index.php
@@ -1,2 +1,9 @@
 <?php
+tideways_xhprof_enable(TIDEWAYS_XHPROF_FLAGS_MEMORY | TIDEWAYS_XHPROF_FLAGS_CPU);
+
 phpinfo();
+
+file_put_contents(
+    sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid() . '.myapplication.xhprof',
+    serialize(tideways_xhprof_disable())
+);
